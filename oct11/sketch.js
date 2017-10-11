a4a7ba3b87fe7d9d5eea39e1eaa389f9f10c3e5a
dwaldie <d.waldie19@gmail.com>
2017-10-11 12:52:24 -0500

    lala

---
+++ oct11/sketch.js
@@ -0,0 +1,25 @@
+ document.ontouchmove = function(event){
+ event.preventDefault();
+}
+
+function setup() {
+ createCanvas(windowWidth,windowHeight);
+ rectMode(CENTER);
+ stroke(0);
+ noFill();
+ strokeWeight(5);
+}
+
+function draw(){
+ background(255);
+ 
+beginShape();
+for(var i=0;i<touches.length;i++){
+vertex(touches[i].x,touches[i].y);
+}
+endShape(CLOSE);
+ 
+}
+
+
+
