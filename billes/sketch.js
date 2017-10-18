469636480fc24ada9036e4369c29335db8957bd3
dwaldie <d.waldie19@gmail.com>
2017-10-18 09:46:29 -0500

    rot

--- billes/sketch.js
+++ billes/sketch.js
@@ -1,9 +1,19 @@
+var ax, vx, px
+
+deviceRotationY*vMultiplier;
+
+
 function setup() {
  createCanvas(windowWidth,windowHeight); 
 }
 
+
 function draw()
-{}
+{
+ellipseMode(CENTER);  
+fill(100);  
+ellipse(50, 50, 30, 30); 
+}
 
 function touchStarted() {
  background(random(0,255),random(0,255),random(0,255));
