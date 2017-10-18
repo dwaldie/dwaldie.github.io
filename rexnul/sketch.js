0f50d54d90dabe1876799b25ee5e83ea3f6a9777
dwaldie <d.waldie19@gmail.com>
2017-10-18 11:29:31 -0500

    vhelp

--- rexnul/sketch.js
+++ rexnul/sketch.js
@@ -1,11 +1,34 @@
+var ax, ay, vx, vy, px, py, s,vMultiplier;
+
 function setup() {
- createCanvas(windowWidth,windowHeight); 
+  createCanvas(windowWidth, windowHeight);
+  ax = 0;
+  ay = 0;
+  vx = 0;
+  vy = 0;
+  px = windowWidth/2;
+  py = windowHeight/2;
+  s = 50;
+  vMultiplier= 0.01;
 }
 
+
 function draw() {
- background(255);
- textSize(40);
- text("Rx: " + rotationX, 100, 100);
- text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
+  textSize(40);
+  text("Rx: " + floor(rotationX), 100, 100);
+  text("Ry: " + floor(rotationY), 100, 150);
+  text("Rz: " + floor(rotationZ), 100, 200);
+  drawBille();
+}
+
+
+function drawBille() {
+  ax=rotationY*vMultiplier;
+  vx+=ax;
+  px+=vx;
+  ay=rotationX*vMultiplier;
+  vy+=ay;
+  py+=vy;
+  fill(100);
+  ellipse(px, py, s, s);
 }
